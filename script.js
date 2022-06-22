59461aaa87e804465a4bd197390e9041edbbd2e9
developerpaco <101013120+developerpaco@users.noreply.github.com>
2022-06-22 13:59:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var header = document.getElementById('Header')
 window.addEventListener('scroll', ()=>{
     var scroll = window.scrollY
     if(scroll>10){
-        header.style.backgroundColor = '#121212'
+        header.style.backgroundColor = '#F5C5B6'
     }else{
         header.style.backgroundColor = 'transparent'
     }
